46c690bfc0c1fa2fc1985b69cfc5f691ea88a8f8
salino3 <programacionflavio@gmail.com>
2023-10-21 14:18:42 +0200

    first commit

---
+++ back/src/pods/user/user.mappers.ts
@@ -0,0 +1,8 @@
+import * as model from '#dals/index.js';
+import * as apiModel from './user.api-model.js';
+
+export const mapUserFromModelToApi = (user: model.User): apiModel.User => ({
+  email: user.email,
+  role: user.role,
+  avatar: user.role
+});
